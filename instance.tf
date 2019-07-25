05bd97738f3a38530c2b7aba028192f4b5fc700a
BobbyKataria <bobby.kataria@hotmail.co.uk>
2019-07-25 16:05:23 +0100

    test2

--- instance.tf
+++ instance.tf
@@ -1,6 +1,5 @@
 resource "aws_instance" "frontend" {
   # ubuntu 18.04
-  count = "1"
   ami = "ami-0c30afcb7ab02233d"
   instance_type = "t2.micro"
   subnet_id = "${aws_subnet.default.id}"
@@ -18,14 +17,12 @@ resource "aws_instance" "frontend" {
       "git clone https://github.com/BobbyKataria/AWS_MEAN_DOCKER.git",
       "cd AWS_MEAN_DOCKER",
       "./install.sh",
-      "docker pull bobbykataria/ui",
-      "docker run -d -p 80:80 bobbykataria/ui"
+      "sudo docker run -d -p 80:80 bobbykataria/ui"
     ]
   }
 }
 resource "aws_instance" "api" {
   # ubuntu 18.04
-  count = "1"
   ami = "ami-0c30afcb7ab02233d"
   instance_type = "t2.micro"
   subnet_id = "${aws_subnet.default.id}"
@@ -44,14 +41,13 @@ resource "aws_instance" "api" {
       "cd AWS_MEAN_DOCKER",
       "./install.sh",
       "cd api",
-      "docker build -t api:latest"
-      "docker run -d -e -p 8080:8080 MONGO_HOST=${aws_instance.default3.private_ip} api:latest" 
+      "sudo docker build -t api:latest .",
+      "sudo docker run -d -e -p 8080:8080 MONGO_HOST=${aws_instance.mongo.private_ip} api:latest" 
     ]
   }
 }
 resource "aws_instance" "mongo" {
   # ubuntu 18.04
-  count = "1"
   ami = "ami-0c30afcb7ab02233d"
   instance_type = "t2.micro"
   subnet_id = "${aws_subnet.default.id}"
@@ -69,8 +65,7 @@ resource "aws_instance" "mongo" {
       "git clone https://github.com/BobbyKataria/AWS_MEAN_DOCKER.git",
       "cd AWS_MEAN_DOCKER",
       "./install.sh",
-      "docker pull mongo:latest",
-      "docker run -d -p 27017:27017 mongo:latest"
+      "sudo docker run -d -p 27017:27017 mongo:latest"
     ]
   }
 }
